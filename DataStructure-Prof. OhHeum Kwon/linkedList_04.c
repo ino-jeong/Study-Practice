ea6c1d6ec7e80f3ff76116f3b6f2f45540fb54ff
INO <inoh.jung@gmail.com>
2017-02-23 22:51:51 +0900

    Update linkedList_04.c

--- DataStructure-Prof. OhHeum Kwon/linkedList_04.c
+++ DataStructure-Prof. OhHeum Kwon/linkedList_04.c
@@ -1,1 +1,46 @@
-//not yet
+//remove i'th node
+//return data of removed node
+
+Node *remove(int index){
+    if(index < 0){
+        return NULL;
+    }
+    if(index == 0){
+        return remove_first();
+    }
+    
+    Node *prev = get_node(index-1);
+    if(prev==NULL){
+        return NULL;
+    }
+    else{
+        return remove_after(prev); //remove_after() will return removed node's address
+    }
+}
+
+
+
+//remove by item
+
+Node *remove(char *item){
+    Node *p = head;
+    Node *q = NULL;
+    
+    while(p!=NULL && strcmp(p->data, item)!=0){
+        q=p;
+        p=p->next;
+    }
+    
+    if(p==NULL){ //end of linked list
+        return NULL;
+    }
+    if(q==NULL){ //at the begining
+        return remove_first();
+    }
+    else{
+        return remove_after(q);
+    }
+}
+
+
+
