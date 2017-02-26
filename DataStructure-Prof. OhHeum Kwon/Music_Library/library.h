1a459e810d9b83741ac5442d0ce4df494487b9f0
INO <inoh.jung@gmail.com>
2017-02-26 12:00:42 +0900

    Update library.h

--- DataStructure-Prof. OhHeum Kwon/Music_Library/library.h
+++ DataStructure-Prof. OhHeum Kwon/Music_Library/library.h
@@ -1,6 +1,44 @@
 #ifndef LIBRARY_H
 #define LIBRARY_H
+#include <stdlib.h>
+#include <string.h>
+#define NUM_CHARS 256
+
+typedef struct artist Artist;
+typedef struct song Song;
+typedef struct snode Snode;
+
+
+struct song{
+    Artist *artist;
+    char *title;
+    char *path;
+    int index;
+};
+
+struct snode{
+    struct snode *next, *prev;
+    Song *song;
+};
+
+struct artist {
+    char *name;
+    struct artist *next;
+    Snode *head, *tail;
+};
+
 
 void add_song(char *artisst, char *title, char *path);
+void initialize();
+Artist *find_artist(char *name);
+Artist *add_artist(char *name);
+Artist *create_artist_instance(char *name);
+Song *create_song_instance(Artist *ptr_artist, char *title, char *path);
+void add_song(char *artist, char *title, char *path);
+void insert_node(Artist *ptr_artist, Snode *ptr_snode);
+void status();
+void print_artist(Artist *p);
+void print_song(Song *ptr_song);
+
 
 #endif
