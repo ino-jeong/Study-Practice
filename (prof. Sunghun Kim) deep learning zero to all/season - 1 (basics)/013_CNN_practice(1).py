8db575e6ff2e794f3466e2b733f1981199ee2c14
INO <inoh.jung@gmail.com>
2017-04-13 11:36:25 +0900

    (CNN lab 1/3 backup...)

---
+++ (prof. Sunghun Kim) deep learning zero to all/season - 1 (basics)/013_CNN_practice(1).py
@@ -0,0 +1,35 @@
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+
+sess = tf.InteractiveSession()
+image = np.array([[[[1], [2], [3]],
+                  [[4], [5], [6]],
+                  [[7], [8], [9]]]], dtype=np.float32)
+
+print('image shape :', image.shape)
+weight = tf.constant([[[[1.]], [[1.]]],
+                      [[[1.]], [[1.]]]])
+print('weight shape :', weight.shape)
+
+conv2d = tf.nn.conv2d(image, weight, strides=[1, 1, 1, 1], padding='VALID')
+# print('****', conv2d, '****', conv2d.shape)
+conv2d_img = conv2d.eval()
+
+print(conv2d_img)
+# print('conv2d image shape :', conv2d_img.shape)
+# conv2d_img = np.swapaxes(conv2d_img, 0, 3)
+
+# for i, one_img in enumerate(conv2d_img):
+#     print(one_img.reshape(2,2))
+#     plt.subplot(1, 2, i+1)
+#     plt.imshow(one_img.reshape(2,2), cmap='gray')
+#     plt.show()
+
+# plt.imshow(image.reshape(3,3), cmap='Greys')
+# plt.show()
+
+test = tf.constant(77, dtype=tf.float32)
+print(test)
+print(test.eval())
+
