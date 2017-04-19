4631a551a27a7212b8768ec93bcd83c556156abe
INO <inoh.jung@gmail.com>
2017-04-19 10:40:34 +0900

    deep learning zero to all (by prof. Sunghun Kim) - CNN basic practice

--- (prof. Sunghun Kim) deep learning zero to all/season - 1 (basics)/013_CNN_practice(1).py
+++ (prof. Sunghun Kim) deep learning zero to all/season - 1 (basics)/013_CNN_practice(1).py
@@ -8,28 +8,29 @@ image = np.array([[[[1], [2], [3]],
                   [[7], [8], [9]]]], dtype=np.float32)
 
 print('image shape :', image.shape)
-weight = tf.constant([[[[1.]], [[1.]]],
-                      [[[1.]], [[1.]]]])
+weight = tf.constant([[[[1., 10., -1.]], [[1., 10., -1.]]],
+                      [[[1., 10., -1.]], [[1., 10., -1.]]]])
 print('weight shape :', weight.shape)
 
-conv2d = tf.nn.conv2d(image, weight, strides=[1, 1, 1, 1], padding='VALID')
-# print('****', conv2d, '****', conv2d.shape)
+conv2d = tf.nn.conv2d(image, weight, strides=[1, 1, 1, 1], padding='SAME')
+print('****', conv2d, '****', conv2d.shape)
 conv2d_img = conv2d.eval()
 
 print(conv2d_img)
-# print('conv2d image shape :', conv2d_img.shape)
-# conv2d_img = np.swapaxes(conv2d_img, 0, 3)
+print('conv2d image shape :', conv2d_img.shape)
+conv2d_img = np.swapaxes(conv2d_img, 0, 3)
+print(conv2d_img)
 
-# for i, one_img in enumerate(conv2d_img):
-#     print(one_img.reshape(2,2))
-#     plt.subplot(1, 2, i+1)
-#     plt.imshow(one_img.reshape(2,2), cmap='gray')
-#     plt.show()
+for i, one_img in enumerate(conv2d_img):
+    print(one_img.reshape(3, 3))
+    plt.subplot(1, 3, i+1)
+    plt.imshow(one_img.reshape(3, 3), cmap='gray')
+    plt.show()
 
-# plt.imshow(image.reshape(3,3), cmap='Greys')
-# plt.show()
-
-test = tf.constant(77, dtype=tf.float32)
-print(test)
-print(test.eval())
-
+# # plt.imshow(image.reshape(3,3), cmap='Greys')
+# # plt.show()
+#
+# test = tf.constant(77, dtype=tf.float32)
+# print(test)
+# print(test.eval())
+#
