338a60591e95f044b0fb8fc9d37a49bf99734df1
INO <inoh.jung@gmail.com>
2017-04-24 14:58:48 +0900

    prob. 1562 stair number

--- baekjoon online judge/01562 stair_number/stair_number(1).py
+++ baekjoon online judge/01562 stair_number/stair_number(1).py
@@ -1,41 +1,45 @@
 '''
 
-d[length][num] : stair number with 'length', end at 'num'
-d[length][num] = d[length - 1][num + 1] + d[length - 1][num - 1]
+d[length][num][S] : stair number of 'length', end at 'num' with used number set 'S'
+d[length][num][S] = d[length - 1][num + 1][S - (num + 1)] + d[length - 1][num - 1][S - (num - 1)]
 iff num + 1 and num - 1 borh in range of 0 <= number <= 9
 
 '''
 
 n = int(input())
+mod = 1000000000
 
-memo_arr = [[0] * 10 for i in range(101)]
+memo_arr = [[[-1] * 1024 for j in range(10)] for i in range(101)]
 
-def get_stair_num(length, end_num):
+def get_stair_num(length, end_num, rest_num_set):
     if length == 1:
-        if end_num != 0:
+        if end_num != 0 and rest_num_set == 0:
             return 1
         else:
             return 0
 
-    if memo_arr[length][end_num] != 0:
-        return memo_arr[length][end_num]
+    if memo_arr[length][end_num][rest_num_set] != -1:
+        return memo_arr[length][end_num][rest_num_set]
 
     result = 0
 
     if end_num + 1 <= 9:
-        result += get_stair_num(length - 1, end_num + 1)
+        before_num = end_num + 1
+        result += get_stair_num(length - 1, end_num + 1, rest_num_set & ~ (1<<before_num)) % mod
 
     if end_num - 1 >= 0:
-        result += get_stair_num(length - 1, end_num - 1)
+        before_num = end_num - 1
+        result += get_stair_num(length - 1, end_num - 1, rest_num_set & ~ (1<<before_num)) % mod
 
-    memo_arr[length][end_num] = result % 1000000000
-    return memo_arr[length][end_num]
+    memo_arr[length][end_num][rest_num_set] = result % mod
+    return memo_arr[length][end_num][rest_num_set]
 
 
 total_result = 0
+start_num_set = int('0b1111111111', 2)
 for i in range(10):
-    total_result += get_stair_num(n, i)
+    total_result += get_stair_num(n, i, start_num_set & ~(1<<i)) % mod
 
-print(total_result)
+print(total_result % mod)
 
 
